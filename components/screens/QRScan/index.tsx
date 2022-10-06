d656194238cddc7b3c30802b083ec385865b8085
Shan Dhiviyarajan <prashasoft@gmail.com>
2022-10-06 10:35:51 +0530

    permission fix

--- components/screens/QRScan/index.tsx
+++ components/screens/QRScan/index.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { View, Text } from "react-native";
-
+import QRCodeScanner from "react-native-qrcode-scanner";
+import { RNCamera } from "react-native-camera";
 function QRScan() {
+  const handleReadQR = (e) => {
+    console.log(e);
+  };
   return (
     <View>
       <Text>QR Scan</Text>
+
+      <QRCodeScanner
+        onRead={handleReadQR}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        topContent={
+          <Text>
+            Go to <Text>wikipedia.org/wiki/QR_code</Text> on your computer and
+            scan the QR code.
+          </Text>
+        }
+      />
     </View>
   );
 }
