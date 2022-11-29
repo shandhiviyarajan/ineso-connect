5be2b82b2bf818832839c1d46eb0e431884daeea
Shan Dhiviyarajan <prashasoft@gmail.com>
2022-11-29 20:22:49 +0530

    Update index.tsx

--- components/screens/GoogleMap/index.tsx
+++ components/screens/GoogleMap/index.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  Alert,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-} from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import MapView, { Callout, MapCallout, Marker } from "react-native-maps";
 import { Image } from "react-native";
 import { useSelector } from "react-redux";
@@ -21,8 +15,8 @@ const DeviceGoogleMaps = () => {
     initialRegion = {
       latitude: 45.781941,
       longitude: 4.748706,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
+      latitudeDelta: 5,
+      longitudeDelta: 5,
     };
   }, [devices]);
 
@@ -38,7 +32,13 @@ const DeviceGoogleMaps = () => {
 
   return (
     <>
-      {/* <View
+      <ScrollView
+        scrollEventThrottle={1}
+        contentContainerStyle={{
+          flex: 1,
+        }}
+      >
+        {/* <View
         style={{
           zIndex: 200,
           flex: 1,
@@ -47,73 +47,95 @@ const DeviceGoogleMaps = () => {
       >
         <Text>{JSON.stringify(devices.data && devices.data[0])}</Text>
       </View> */}
-      <View
-        style={{
-          flex: 1,
-          ...StyleSheet.absoluteFillObject,
-        }}
-      >
-        <MapView
-          showsUserLocation={true}
-          customMapStyle={[]}
-          zoomEnabled={true}
-          initialRegion={{
-            latitude:
-              devices &&
+        <>
+          <View
+            style={{
+              flex: 0.25,
+            }}
+          >
+            {devices &&
               devices.data &&
-              devices.data[0] &&
-              devices.data[0].metadata &&
-              devices.data[0].metadata.gpsLocation.latitude
-                ? devices.data[0].metadata.gpsLocation.latitude
-                : 45.781941,
-            longitude:
-              devices &&
-              devices.data &&
-              devices.data[0] &&
-              devices.data[0].metadata &&
-              devices.data[0].metadata.gpsLocation.longitude
-                ? devices.data[0].metadata.gpsLocation.longitude
-                : 4.748706,
-            latitudeDelta: 0.003,
-            longitudeDelta: 0.003,
-          }}
-          style={{
-            ...StyleSheet.absoluteFillObject,
-          }}
-        >
-          {devices &&
-            devices.data &&
-            devices.data.map((device, i) => (
-              <Marker
-                key={"map-marker" + i}
-                description={device.metadata.name}
-                coordinate={{
-                  longitude: device.metadata.gpsLocation.longitude,
-                  latitude: device.metadata.gpsLocation.latitude,
-                }}
-              >
-                <View
+              devices.data.map((d) => (
+                <Text
                   style={{
-                    width: 52,
-                    height: 52,
-                    backgroundColor: SystemColors.primary,
-                    borderRadius: 44,
-                    elevation: 5,
-                    shadowOpacity: 0.15,
-                    shadowColor: "rgba(0,0,0,.5)",
-                    justifyContent: "center",
-                    alignItems: "center",
+                    padding: 24,
                   }}
                 >
-                  <Image
-                    source={GenerateImage(device.metadata.model)}
-                    style={{ width: 32, height: 32, tintColor: "#fff" }}
-                  />
-                </View>
-              </Marker>
-            ))}
-        </MapView>
-      </View>
+                  {d.metadata.gpsLocation.longitude +
+                    " - " +
+                    d.metadata.gpsLocation.latitude}
+                </Text>
+              ))}
+          </View>
+          <View
+            style={{
+              flex: 1,
+              // ...StyleSheet.absoluteFillObject,
+            }}
+          >
+            <MapView
+              showsUserLocation={true}
+              customMapStyle={[]}
+              zoomEnabled={true}
+              initialRegion={{
+                latitude:
+                  devices &&
+                  devices.data &&
+                  devices.data[0] &&
+                  devices.data[0].metadata &&
+                  devices.data[0].metadata.gpsLocation.latitude
+                    ? devices.data[0].metadata.gpsLocation.latitude
+                    : 45.781941,
+                longitude:
+                  devices &&
+                  devices.data &&
+                  devices.data[0] &&
+                  devices.data[0].metadata &&
+                  devices.data[0].metadata.gpsLocation.longitude
+                    ? devices.data[0].metadata.gpsLocation.longitude
+                    : 4.748706,
+                latitudeDelta: 5,
+                longitudeDelta: 5,
+              }}
+              style={{
+                ...StyleSheet.absoluteFillObject,
+              }}
+            >
+              {devices &&
+                devices.data &&
+                devices.data.map((device, i) => (
+                  <Marker
+                    key={"map-marker" + i}
+                    description={device.metadata.name}
+                    coordinate={{
+                      longitude: device.metadata.gpsLocation.longitude,
+                      latitude: device.metadata.gpsLocation.latitude,
+                    }}
+                  >
+                    <View
+                      style={{
+                        width: 52,
+                        height: 52,
+                        backgroundColor: SystemColors.primary,
+                        borderRadius: 44,
+                        elevation: 5,
+                        shadowOpacity: 0.15,
+                        shadowColor: "rgba(0,0,0,.5)",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Image
+                        source={GenerateImage(device.metadata.model)}
+                        style={{ width: 32, height: 32, tintColor: "#fff" }}
+                      />
+                    </View>
+                  </Marker>
+                ))}
+            </MapView>
+          </View>
+        </>
+      </ScrollView>
     </>
   );
 };
