315dd3f77e2d0e436d3f4931443242298b957144
Shan Dhiviyarajan <prashasoft@gmail.com>
2022-12-07 03:07:43 +0530

    Update interceptors.ts

--- core/interceptors/interceptors.ts
+++ core/interceptors/interceptors.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 import { Message } from "../../components/molecules/Toast";
 import jwt_decode from "jwt-decode";
 //set default api url
-// BASE_URL = "https://connect-staging.inesocompany.com/api";
-let BASE_URL = "https://connect.inesocompany.com/api";
+let BASE_URL = "https://connect-staging.inesocompany.com/api";
+//let BASE_URL = "https://connect.inesocompany.com/api";
 let TIME_OUT = 50000;
 let ACCESS_TOKEN: any = false;
 let axiosRequest = axios.create({
